23b03eefedc0574751da96654b3fbf4d34a78537
sherwin-77 <sherwinak1@gmail.com>
2023-09-09 18:49:59 +0700

    Removing canvas dependency

--- src/app/api/image/route.tsx
+++ src/app/api/image/route.tsx
@@ -1,6 +1,6 @@
 import { load } from "cheerio"
 import { NextResponse, type NextRequest } from "next/server"
-import { createCanvas } from 'canvas'
+// import { createCanvas } from 'canvas'
 
 
 export async function GET(request: NextRequest) {
@@ -8,6 +8,7 @@ export async function GET(request: NextRequest) {
     const search = searchParams.get("id") ?? searchParams.get("username")
     if(!search) return NextResponse.json("Parameter id or username required", {status: 400})
 
+    return NextResponse.json("Come back later", {status: 501})
     // const page = await fetch(`${process.env.API_URL}/users/${search}`, {
     //     next: {revalidate: 15}
     // })
@@ -19,19 +20,19 @@ export async function GET(request: NextRequest) {
     // const data = res.attr("data-initial-data")
     // if(!data) return NextResponse.json("Missing data", {status: 404})
 
-    const canvas = createCanvas(250, 250)
-    const ctx = canvas.getContext('2d')
-    ctx.font = '30px '
-    ctx.fillStyle = "white";
-    ctx.save()
-    ctx.fillStyle = "gray";
-    ctx.fillRect(0, 0, 250, 250);
+    // const canvas = createCanvas(250, 250)
+    // const ctx = canvas.getContext('2d')
+    // ctx.font = '30px '
+    // ctx.fillStyle = "white";
+    // ctx.save()
+    // ctx.fillStyle = "gray";
+    // ctx.fillRect(0, 0, 250, 250);
 
-    ctx.restore()
-    ctx.fillText("SoonTM", 50, 50)
-    const buffer = canvas.toBuffer()
+    // ctx.restore()
+    // ctx.fillText("SoonTM", 50, 50)
+    // const buffer = canvas.toBuffer()
 
-    return new  NextResponse(buffer, {status: 200, headers: {
-        "content-type": "image/png"
-    }})
+    // return new  NextResponse(buffer, {status: 200, headers: {
+    //     "content-type": "image/png"
+    // }})
 }
